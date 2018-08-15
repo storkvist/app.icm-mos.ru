95b7c0b30bb71fb423b39513960938e00e55b54c
Vitaly Shlyaga <vitaly@shlyaga.ru>
2018-08-15 15:41:29 +0300

    .

--- app/controllers/kits_controller.rb
+++ app/controllers/kits_controller.rb
@@ -5,7 +5,7 @@ class KitsController < ApplicationController
     respond_to do |format|
       format.html
       format.json do
-        render json: @kits.to_json(only: %i[article name_en name_ru kit_description_en kit_description_ru prototype_description_en
+        render json: @kits.to_json(only: %i[article year name_en name_ru kit_description_en kit_description_ru prototype_description_en
                                             prototype_description_ru packaging_description_en packaging_description_ru])
       end
       format.xlsx
