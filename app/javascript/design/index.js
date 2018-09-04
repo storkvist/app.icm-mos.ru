f136754a58b2e547f2b1af072e8fdc119adb4253
Vitaly Shlyaga <vitaly@shlyaga.ru>
2018-09-04 19:37:21 +0300

    sticky header

--- app/javascript/design/index.js
+++ app/javascript/design/index.js
@@ -1,9 +1,10 @@
 import './bulma.scss'
 import './fontawesome.scss'
 
-import './utilities.scss';
+import './utilities.scss'
 import './layout.scss'
 import './forms.scss'
+import './tables.scss'
 
 import './kits.scss'
 import './scales.scss'
