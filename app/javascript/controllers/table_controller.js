a28b61c7aace5375e733b81bd89606238ff488b6
Vitaly Shlyaga <vitaly@shlyaga.ru>
2018-10-08 22:36:31 +0300

    .

--- app/javascript/controllers/table_controller.js
+++ app/javascript/controllers/table_controller.js
@@ -1,11 +1,24 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-  static targets = ['filter'];
+  static targets = ['filter', 'checkAll', 'checkbox'];
 
   filter() {
     this.filterTargets.forEach((input) => {
       console.log(input);
     });
+  }
+
+  checkAll() {
+    const checked = this.checkAllTarget.checked;
+    this.checkboxTargets.forEach((checkbox) => {
+      checkbox.checked = checked;
+    });
+  }
+
+  uncheckCheckAll(event) {
+    if (!event.currentTarget.checked) {
+      this.checkAllTarget.checked = false;
+    }
   }
 }
