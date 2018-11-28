981c8dbf8b0fd39a3266c119d8e8235c3bfe9e2e
Vitaly Shlyaga <vitaly@shlyaga.ru>
2018-11-28 14:36:26 +0300

    корректная обработка и генерация превью

--- app/controllers/kits_controller.rb
+++ app/controllers/kits_controller.rb
@@ -21,10 +21,13 @@ class KitsController < ApplicationController
     if kit_params[:box_art].present?
       @kit.box_art.attach(kit_params[:box_art])
 
-      @kit.preview.attach(kit_params[:box_art]) if kit_params[:preview].blank?
+      if kit_params[:preview].present?
+        @kit.preview.attach(kit_params[:preview])
+      else
+        @kit.preview.attach(kit_params[:box_art])
+      end
     end
 
-    @kit.box_art.attach(kit_params[:preview]) if kit_params[:preview].present?
     @kit.assembled_photo.attach(kit_params[:assembled_photo]) if kit_params[:assembled_photo].present?
     @kit.packaging_photo.attach(kit_params[:packaging_photo]) if kit_params[:packaging_photo].present?
 
@@ -47,7 +50,7 @@ class KitsController < ApplicationController
 
   def kit_params
     params.require(:kit).permit(:article, :barcode, :assembled_photo, :box_art, :kit_description_en, :kit_description_ru, :manual, :name_en,
-                                :name_ru, :packaging_description_en, :packaging_description_ru, :packaging_photo, :prototype_description_en,
-                                :prototype_description_ru, :scale_id, :year, images: [])
+                                :name_ru, :packaging_description_en, :packaging_description_ru, :packaging_photo, :preview,
+                                :prototype_description_en, :prototype_description_ru, :scale_id, :year, images: [])
   end
 end
